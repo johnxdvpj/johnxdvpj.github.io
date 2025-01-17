7568504507ec304c64302ad5b827274ec2492036
johnxdvpj <37866793+johnxdvpj@users.noreply.github.com>
2025-01-17 23:30:12 +0100

    test saving

--- js/wedding.js
+++ js/wedding.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         console.log(data[0].user);
         $.ajax({
             type: 'POST',
-            url: '../json/wedding.json',
+            url: 'https://api.jsonbin.io/v3/b/678ad996ad19ca34f8eed671',
             data: JSON.stringify(data),
             success: function (response) {
                 console.log('Data saved successfully:', response);
