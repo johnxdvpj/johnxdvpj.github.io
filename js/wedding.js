f04dfc95a2bc014d1b33f92e98a3f1aef9f6fe78
johnxdvpj <37866793+johnxdvpj@users.noreply.github.com>
2025-01-17 23:19:33 +0100

    please

--- js/wedding.js
+++ js/wedding.js
@@ -2,5 +2,16 @@ $(document).ready(function () {
     $.getJSON("../json/wedding.json", function (data) {
         data[0].user = "Hello"
         console.log(data[0].user);
+        $.ajax({
+            type: 'POST',
+            url: '../json/wedding.json',
+            data: JSON.stringify(data),
+            success: function (response) {
+                console.log('Data saved successfully:', response);
+            },
+            error: function (error) {
+                console.error('Error saving data:', error);
+            }
+        });
     });
 });
