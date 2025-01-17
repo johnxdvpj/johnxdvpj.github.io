eb374fe27d3f804fb9b874294bd39b75e40c3afb
johnxdvpj <37866793+johnxdvpj@users.noreply.github.com>
2025-01-17 23:13:43 +0100

    delete php and save json through ajax

--- js/wedding.js
+++ js/wedding.js
@@ -3,10 +3,8 @@ $(document).ready(function () {
         data[0].user = "Hello"
         console.log(data[0].user);
         $.ajax({
-            url: '../php/save.php', // Server-side script to handle saving 
             type: 'POST',
-            contentType: 'json',
-            async: false,
+            url: '../json/wedding.json',
             data: JSON.stringify(data),
             success: function (response) {
                 console.log('Data saved successfully:', response);
