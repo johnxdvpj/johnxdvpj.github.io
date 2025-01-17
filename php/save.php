8c58538593d19151b152751766a92a444c18af9c
johnxdvpj <37866793+johnxdvpj@users.noreply.github.com>
2025-01-17 22:48:06 +0100

    change path in php

--- php/save.php
+++ php/save.php
@@ -1,5 +1,5 @@
 <?php
-$myFile = "general.json";
+$myFile = "https://jsonhost.com/json/f21d790128db3973f683655d1e58d6a0";
 $fh = fopen($myFile, 'w') or die("can't open file");
 $stringData = $_GET["data"];
 fwrite($fh, $stringData);
