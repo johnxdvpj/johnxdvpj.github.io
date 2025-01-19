9fa3e3e43304ba211606cf5555cf84724615a231
johnxdvpj <37866793+johnxdvpj@users.noreply.github.com>
2025-01-19 14:58:39 +0100

    test

--- js/login.js
+++ js/login.js
@@ -1,7 +1,11 @@
 function CheckJson() {
     $.getJSON("../json/wedding.json", function (data) {
+        var nutzer = $('#nutzer').val();
         $.each(data, function (key, val) {
             console.log(data[key].user)
+            if (data[key].user == nutzer) {
+                console.log("success")
+            }
         });
     });
 };
