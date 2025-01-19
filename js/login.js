5845df3fc8ee86b8418898b91124f2030478d2ee
johnxdvpj <37866793+johnxdvpj@users.noreply.github.com>
2025-01-19 15:04:09 +0100

    test

--- js/login.js
+++ js/login.js
@@ -1,10 +1,11 @@
 function CheckJson() {
     $.getJSON("../json/wedding.json", function (data) {
         var nutzer = $('#nutzer').val();
+        var password = $('#password').val();
         $.each(data, function (key, val) {
             console.log(data[key].user)
             if (data[key].user == nutzer) {
-                console.log("success")
+                console.log(key)
                 return false;
             }
         });
